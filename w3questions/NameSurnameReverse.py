a227c68d1745161688fa56830bd01e90ee085458
Jesal <jesalchana23@gmail.com>
2022-03-16 19:38:49 +0200

    Added w3 challanged

---
+++ w3questions/NameSurnameReverse.py
@@ -0,0 +1,9 @@
+#Write a Python program which accepts the user's first and last name and print them in reverse order
+#  with a space between them
+
+print('What is your name?')
+name = input()
+print('What is your surname?')
+surname = input()
+
+print(surname + ' ' + name)
---
+++ w3questions/radiusOfCircle.py
@@ -0,0 +1,9 @@
+import math
+
+print('What is the radius of the circle you want the area for?...')
+
+radius = int(input())
+
+areaOfCircle = math.pi*radius*radius
+
+print(areaOfCircle)
