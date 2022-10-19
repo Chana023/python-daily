7b1ec9bfe3ddc2e662c8815923af4b116bc7cee3
Jesal <jesalchana23@gmail.com>
2022-10-19 20:56:28 +0200

    19-10-2022

---
+++ hackerRank/string_validator.py
@@ -0,0 +1,40 @@
+from curses.ascii import isalpha, isdigit
+
+
+if __name__ == '__main__':
+    s = input()
+
+    for char in s:
+        if char.isalnum():
+            print('True')
+            break
+    else:
+        print('False')
+    
+    for char in s:
+        if char.isalpha():
+            print('True')
+            break
+    else:
+        print('False')
+
+    for char in s:
+        if char.isdigit():
+            print('True')
+            break
+    else:
+        print('False')
+
+    for char in s:
+        if char.islower():
+            print('True')
+            break
+    else:
+        print('False')
+
+    for char in s:
+        if char.isupper():
+            print('True')
+            break
+    else:
+        print('False') 
