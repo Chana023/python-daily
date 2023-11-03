4d3019c848edbd8255cea5e2b282f4b20a3690cd
Chana023 <jesalchana23@gmail.com>
2023-11-03 10:31:45 +0200

    Completed alphabet Rangoli challenge

--- hackerRank/alphabet_rangoli.py
+++ hackerRank/alphabet_rangoli.py
@@ -4,14 +4,30 @@ def print_rangoli(size):
     width = (4*size) - 3
     length = (2*size) - 1
     all_letters = []
+    final_string = ''
 
     for letter in range(max_letter, 96, -1):
         all_letters.append(chr(letter))
 
     
-
     # Top pyramid
+    line_list = []
+    reverse_line_list = []
+    bottom_lines  =[]
+    for x in range(0,size-1,1):
+        line_list = all_letters[0:x+1]
 
+        reverse_line_list = line_list.copy()
+        reverse_line_list.reverse()
+        reverse_line_list.pop(0)
+
+        line_list = line_list + reverse_line_list
+
+        line = '-'.join(line_list)
+        line = line.center(width,'-')
+
+        bottom_lines.append(line)
+        final_string = final_string + line + '\n'
 
 
     # Middle row generation 
@@ -20,16 +36,21 @@ def print_rangoli(size):
     all_letters_minus_first.reverse()
     all_letters_minus_first.pop(0)
 
-
     middle_row = all_letters + all_letters_minus_first
+    middle_row_value = '-'.join(middle_row)  
+    
+    final_string = final_string + middle_row_value + '\n'
 
-    middle_row_value = '-'.join(middle_row)
     
 
-    print(middle_row_value)
-
     # Bottom pyramid
-        
+    bottom_lines.reverse()
+
+    for y in range(0,size-1,1):
+        final_string = final_string + bottom_lines[y] + '\n'
+
+    return final_string
+
 
         
 
