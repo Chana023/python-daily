f7e00b9fd817bdf4bf900b35b06ac76b90a602a2
Chana023 <jesalchana23@gmail.com>
2023-11-12 12:14:00 +0200

    Completed number of words

---
+++ hackerRank/word_order.py
@@ -0,0 +1,23 @@
+# https://www.hackerrank.com/challenges/word-order/problem
+
+number_words = int(input())
+
+word_dict = {}
+
+for value in range(number_words) :
+    word = input()
+
+    if word in word_dict:
+        word_dict[word] += 1
+    else:
+        word_dict[word] = 1
+
+print(len(word_dict))
+answer = ' '.join(f'{value}' for key, value in word_dict.items())
+# answer = ' '.join(answer) 
+
+print(answer)
+
+
+
+
