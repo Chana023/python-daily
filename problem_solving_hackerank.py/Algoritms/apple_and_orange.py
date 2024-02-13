853c1603d88755cce0250177ced8374f14c95ca4
Chana023 <jesalchana23@gmail.com>
2024-02-13 18:05:18 +0200

    Completed apple and orange algo challenge

---
+++ problem_solving_hackerank.py/Algoritms/apple_and_orange.py
@@ -0,0 +1,60 @@
+# https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # Write your code here
+    diff_apple = []
+    diff_orange = []
+
+    diff_apple = [ a + x for x in apples]
+    diff_orange = [ b + y for y in oranges ]
+
+    apple_count = [ x for x in diff_apple if x >= s and x <= t ]
+    orange_count = [ x for x in diff_orange if x >= s and x <= t]
+    
+    print(len(apple_count))
+    print(len(orange_count))
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
