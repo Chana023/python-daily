9a2e5b7e0c4e0bdd708bd98a92869f75e8820f52
Chana023 <jesalchana23@gmail.com>
2024-03-12 11:39:15 +0200

    Completed viral advertising challenge

---
+++ problem_solving_hackerank.py/Algoritms/viral_advertising.py
@@ -0,0 +1,45 @@
+# https://www.hackerrank.com/challenges/strange-advertising/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+
+    prev_recep = 5
+    total_like = 0
+
+    for day in range(1, n+1, 1):
+
+        
+
+        likes = math.floor(prev_recep/2)
+        total_like = total_like + likes
+
+        recepients = math.floor(prev_recep/2) * 3
+
+        print(day, likes, total_like)
+        
+
+        prev_recep = recepients
+
+
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
