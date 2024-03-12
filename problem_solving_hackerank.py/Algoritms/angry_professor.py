e1640a094859f62ed744769f11396d61ec85c55a
Chana023 <jesalchana23@gmail.com>
2024-03-12 10:39:23 +0200

    Completed angry professor algo challenge

---
+++ problem_solving_hackerank.py/Algoritms/angry_professor.py
@@ -0,0 +1,46 @@
+# https://www.hackerrank.com/challenges/angry-professor/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'angryProfessor' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY a
+#
+
+def angryProfessor(k, a):
+    # Write your code here
+    count = 0
+
+    for value in a:
+        if value <= 0:
+            count = count + 1
+
+    if count >= k:
+        return 'YES'
+    elif count < k:
+        return 'NO'
+
+if __name__ == '__main__':
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
