40c7750867e9f64e40bd44c527bea9327e2bd202
Chana023 <jesalchana23@gmail.com>
2024-03-12 11:43:50 +0200

    Competed viral advertising algo challenge

--- problem_solving_hackerank.py/Algoritms/viral_advertising.py
+++ problem_solving_hackerank.py/Algoritms/viral_advertising.py
@@ -23,17 +23,14 @@ def viralAdvertising(n):
 
     for day in range(1, n+1, 1):
 
-        
-
         likes = math.floor(prev_recep/2)
         total_like = total_like + likes
-
         recepients = math.floor(prev_recep/2) * 3
-
-        print(day, likes, total_like)
         
 
         prev_recep = recepients
+
+    return total_like
 
 
 
