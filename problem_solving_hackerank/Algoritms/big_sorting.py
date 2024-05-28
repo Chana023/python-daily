09a1c65b1f02a13d4ad5878aa1475827d14352ce
Chana023 <jesalchana23@gmail.com>
2024-05-28 15:06:52 +0200

    Completed big sorting algorithm algo challenge

---
+++ problem_solving_hackerank/Algoritms/big_sorting.py
@@ -0,0 +1,34 @@
+# https://www.hackerrank.com/challenges/big-sorting/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bigSorting' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts STRING_ARRAY unsorted as parameter.
+#
+
+def bigSorting(unsorted):
+    # Write your code here
+    unsorted.sort(key = lambda x: (len(x), x))
+
+    print(unsorted)
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
