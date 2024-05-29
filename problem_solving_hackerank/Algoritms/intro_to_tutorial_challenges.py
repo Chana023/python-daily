279ddf14344c7104a213ba39f8b61677699f5721
Chana023 <jesalchana23@gmail.com>
2024-05-29 09:59:19 +0200

    Completed intro to tutorial algo challenges

---
+++ problem_solving_hackerank/Algoritms/intro_to_tutorial_challenges.py
@@ -0,0 +1,33 @@
+# https://www.hackerrank.com/challenges/tutorial-intro/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'introTutorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER V
+#  2. INTEGER_ARRAY arr
+#
+
+def introTutorial(V, arr):
+    # Write your code here
+    print(arr.index(V))
+
+if __name__ == '__main__':
+
+    V = int(input().strip())
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = introTutorial(V, arr)
+
