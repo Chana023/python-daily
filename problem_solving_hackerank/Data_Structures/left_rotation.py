06c518a23919b30e800555c276d331e8dc57313e
Chana023 <jesalchana23@gmail.com>
2024-08-15 18:18:03 +0200

    Completed Data structures left rotation array chall

---
+++ problem_solving_hackerank/Data_Structures/left_rotation.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/array-left-rotation/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    arr = list(arr)
+    new_arr = []
+
+    new_arr = arr[d:] + arr[:d]
+
+    print(new_arr)
+
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
