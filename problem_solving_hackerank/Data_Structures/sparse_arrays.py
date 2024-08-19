9221f30dbfbe7efd4b70621815dfe517eae1e25d
Chana023 <jesalchana23@gmail.com>
2024-08-19 18:54:15 +0200

    Completed sparse arrays algo challenge

---
+++ problem_solving_hackerank/Data_Structures/sparse_arrays.py
@@ -0,0 +1,53 @@
+# https://www.hackerrank.com/challenges/sparse-arrays/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    # Write your code here
+    counter = 0
+    result = []
+
+    for query in queries:
+        for s in stringList:
+            if query == s:
+                counter += 1
+
+        result.append(counter)
+        counter = 0
+
+    print(result)
+
+if __name__ == '__main__':
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
