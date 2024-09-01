b7f7cb292935fb09daf68061b8469da8a574619a
Chana023 <jesalchana23@gmail.com>
2024-09-01 19:26:18 +0200

    Completed compare to linked lists data structures challenge

---
+++ problem_solving_hackerank/Data_Structures/compare_two_linked_lists.py
@@ -0,0 +1,85 @@
+# https://www.hackerrank.com/challenges/compare-two-linked-lists/problem?isFullScreen=true
+
+#!/bin/python3
+
+import os
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        print(str(node.data))
+
+        node = node.next
+
+        if node:
+            print(sep)
+
+# Complete the compare_lists function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def compare_lists(llist1, llist2):
+    while llist1 is not None and llist2 is not None:
+        print(llist1.data, llist2.data)
+        if llist1.data != llist2.data:
+            print(0)
+            return
+        
+        llist1 = llist1.next
+        llist2 = llist2.next
+
+    if llist1 is None and llist2 is None:
+        print(1)
+        return
+
+
+if __name__ == '__main__':
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        result = compare_lists(llist1.head, llist2.head)
+
